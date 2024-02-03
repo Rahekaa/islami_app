a54fad8acf2899650c3433c4dc69e3d90586fab5
rahek0 <oenas1810@gmail.com>
2024-02-03 18:44:17 +0200

    added bottom navigation bar in home screen and background image.

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/home/hadeth/hadeth_tab.dart';
+import 'package:islami_app/home/quran/quran_tab.dart';
+import 'package:islami_app/home/radio/radio_tab.dart';
+import 'package:islami_app/home/sebha/sebha_tab.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName = 'home-screen';
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Stack(
+      children: [
+        Image.asset(
+          'assets/images/background.png',
+          height: double.infinity,
+          width: double.infinity,
+          fit: BoxFit.fill,
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text(
+              'Islami',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          bottomNavigationBar: Theme(
+            data: Theme.of(context).copyWith(),
+            child: BottomNavigationBar(
+                currentIndex: selectedIndex,
+                onTap: (index) {
+                  selectedIndex = index;
+                  setState(() {});
+                },
+                items: [
+                  BottomNavigationBarItem(
+                      icon:
+                          ImageIcon(AssetImage('assets/images/icon_quran.png')),
+                      label: 'Quran'),
+                  BottomNavigationBarItem(
+                      icon: ImageIcon(
+                          AssetImage('assets/images/icon_hadeth.png')),
+                      label: 'Hadeth'),
+                  BottomNavigationBarItem(
+                      icon:
+                          ImageIcon(AssetImage('assets/images/icon_sebha.png')),
+                      label: 'Sebha'),
+                  BottomNavigationBarItem(
+                      icon:
+                          ImageIcon(AssetImage('assets/images/icon_radio.png')),
+                      label: 'Radio'),
+                ]),
+          ),
+          body: tabs[selectedIndex],
+        ),
+      ],
+    );
   }
+
+  List<Widget> tabs = [QuranTab(), HadethTab(), SeebhaTab(), RadioTab()];
 }
